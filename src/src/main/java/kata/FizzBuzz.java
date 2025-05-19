e31700e4551f8e6fac1238c2bb236ac1c099aa14
Thomas Schaller <th.schaller@gmail.com>
2025-05-19 10:26:00 +0200

    1er commit TDD.

---
+++ src/src/main/java/kata/FizzBuzz.java
@@ -0,0 +1,21 @@
+package kata;
+
+public class FizzBuzz {
+
+    public static void main(String [] args){
+
+    }
+
+    /**
+     * fonction qui en fonction du paramètre renvoie:
+     * - fizz si le nombre en paramètre est divisible par 3,
+     * - buzz si le nombre est divisible par 5
+     * - le nombre en paramètre dans les autres cas
+     * @param nombre
+     * @return
+     */
+    int convert(int nombre)
+    {
+        return 1;
+    }
+}
---
+++ src/src/main/java/kata/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+package kata;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    /**
+     * Lorsque il y a 1 en entrée, la fonction renvoie 1
+     */
+    @Test
+    public void convertAvec1()
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+         Assert.assertEquals(1, fizzBuzz.convert(1));
+    }
+
+
+}
